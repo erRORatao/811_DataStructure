bf05b50258cfeb0cec3d6deed763ffc41aeb1a76
atao6699 <68963202+atao6699@users.noreply.github.com>
2021-08-12 23:10:11 +0800

    Add files via upload

--- Basics/二叉树.cpp
+++ Basics/二叉树.cpp
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define Maxsize 10
+
 typedef struct BiTNode {
 	int data;
 	BiTNode* Lchild, * Rchild;
@@ -58,11 +60,74 @@ void PreOrder(BiTree B) {
 	}
 }
 
+//偷懒Boy，这里直接抄队列的代码，所以看起来变量看起来有点奇怪
+typedef struct {
+	BiTree Node[50];
+	int front, rear;
+} BiT_Q;
+
+//将层次遍历的队列初始化
+void InitSqueue(BiT_Q& Q) {
+	Q.front = 0;
+	Q.rear = 0;
+}
+
+//判断层次遍历的队列内是否还有结点
+bool StackEmpty(BiT_Q Q) {
+	if (Q.front == Q.rear)
+		return false;
+	return true;
+}
+
+//分别在递归时调用进队和出队
+
+//顺序队列进队
+bool Entrance(BiT_Q& Q, BiTree x) {
+	if (Q.front == Q.rear + 1 || Q.front == (Q.rear + 1) % Maxsize) //这里需要空一个元素不能储存，队满队空的状态无法判断；也可以使用一个计算器
+		return false;
+	Q.Node[Q.rear] = x;
+	Q.rear = (Q.rear + 1) % Maxsize;
+	return true;
+}
+
+//顺序队列出队
+bool Goout(BiT_Q& Q, BiTree& x) {
+	if (StackEmpty(Q)) {
+		x = Q.Node[Q.front];
+		Q.front = (Q.front + 1) % Maxsize;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+//层次遍历
+void Leveltraversal(BiTree B) {
+	BiT_Q Q;
+	BiTNode* node;
+	InitSqueue(Q);
+	Entrance(Q, B);
+	while (StackEmpty(Q)) {
+		Goout(Q, node);
+		printf("%d ", node->data);
+		if (node->Lchild) {
+			Entrance(Q, node->Lchild);
+		}
+		if (node->Rchild) {
+			Entrance(Q, node->Rchild);
+		}
+	}
+}
+
+
 int main() {
 	int p[8] = { 28, 16, 13, 22, 30, 29, 43 };
 	int i[7] = { 13, 16, 22, 28, 29, 30, 43 };
 	int start = 0;
 	BiTree B = (BiTree)Preorder_To_Inorder(start, 0, 7, i, p);
 	PostOrder(B);
+	printf("\n");
+	Leveltraversal(B);
 	return 0;
 }
